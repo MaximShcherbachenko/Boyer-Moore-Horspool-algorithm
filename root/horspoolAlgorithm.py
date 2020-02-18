55b569837d2b21783ec3b14a0f304632d24b8d1d
igortereshchenko <tereshchenko.igor@gmail.com>
2020-02-18 18:27:51 +0200

    Initial commit

---
+++ root/horspoolAlgorithm.py
@@ -0,0 +1,49 @@
+userText = input("Enter the text:\n")   # user input Text
+userPattern = input("Enter the pattern:\n")  # user input pattern
+
+valueTable = {}  # dictionary for values for table
+counter = len(userPattern) - 1
+counter_1 = 0
+
+
+# creating the table of rolls
+for i in range(0, len(userPattern) - 1):
+    valueTable[userPattern[i]] = counter
+    counter -= 1
+
+
+# Realization of algorithm
+textPointer = 0
+while textPointer < (len(userText) - len(userPattern) + 1):
+    if len(userText) - len(userPattern) != 0:
+        flag = 0  # quantity match
+
+        # gui
+        print(userText)
+        print(" " * textPointer + userPattern)
+
+        # realization
+        for patternPointer in range(len(userPattern)):
+            if userText[textPointer + patternPointer] == userPattern[patternPointer]:
+                flag += 1
+            elif str(userText[textPointer + len(userPattern) - 1]) in valueTable.keys():
+                textPointer += valueTable[userText[textPointer + len(userPattern) - 1]]
+                break
+            else:
+                textPointer += len(userPattern)
+                break
+        # prerequisite check
+        if flag == len(userPattern):
+            counter_1 = textPointer
+            break
+    else:
+        print("No matches:(\n")
+        break
+
+print("\n\n")
+
+# print the result 
+if counter_1 != 0:
+    print("Start position of the pattern =", counter_1)
+else:
+    print("No matches:(\n")
